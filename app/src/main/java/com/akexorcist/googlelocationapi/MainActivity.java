020f840eecff69fec3a96319f113dd87aa13e3ee
sAyMaYz <sAyMaYz>
2016-05-26 16:21:58 +0700

    Initial commit

---
+++ app/src/main/java/com/akexorcist/googlelocationapi/MainActivity.java
@@ -0,0 +1,87 @@
+package com.akexorcist.googlelocationapi;
+
+import android.location.Location;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
+    private GoogleApiClient googleApiClient;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.text_view);
+
+        // Create Google API Client instance
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Connect to Google API Client
+        googleApiClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (googleApiClient != null && googleApiClient.isConnected()) {
+            // Disconnect Google API Client if available and connected
+            googleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        // Do something when connected with Google API Client
+
+        LocationAvailability locationAvailability = LocationServices.FusedLocationApi.getLocationAvailability(googleApiClient);
+        if (locationAvailability.isLocationAvailable()) {
+            // Call Location Services
+            LocationRequest locationRequest = new LocationRequest()
+                    .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                    .setInterval(5000);
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+        } else {
+            // Do something when Location Provider not available
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // Do something when Google API Client connection was suspended
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // Do something when Google API Client connection failed
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // Do something when got new current location
+        textView.setText("Latitude : " + location.getLatitude() + "\n" +
+                "Longitude : " + location.getLongitude());
+    }
+}
